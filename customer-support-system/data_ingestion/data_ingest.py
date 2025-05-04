19a763d776ab2a4812640562f289877051825556
anurag26-q <anuragparashar111@gmail.com>
2025-05-04 13:49:10 +0530

    env setup 1

--- customer-support-system/data_ingestion/data_ingest.py
+++ customer-support-system/data_ingestion/data_ingest.py
@@ -4,8 +4,15 @@ import os
 import pandas as pd
 from data_ingestion.data_transform import Data_Converter
 
+load_dotenv()
+ASTRA_DB_API_ENDPOINT=os.getenv('ASTRA_DB_API_ENDPOINT')
+ASTRA_DB_APPLICATION_TOKEN=os.getenv('ASTRA_DB_APPLICATION_TOKEN')
+ASTRA_DB_KEYSPACE=os.getenv('ASTRA_DB_KEYSPACE')
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 
-class Ingest_Data:
+
+
+class Ingest_Data: 
 
     def __init__(self):
         print('hello world')
