d5bd16ebd3a444b60438586bc2bdfaa22ee1aad5
anurag26-q <anuragparashar111@gmail.com>
2025-05-04 12:37:40 +0530

    class scelaton created

---
+++ customer-support-system/data_ingestion/data_transform.py
@@ -0,0 +1,10 @@
+import  pandas as pd
+from langchain_core.documents import Document
+
+
+class Data_Converter:
+    def __init__(self):
+        pass
+
+    def data_trasformation(self):
+        pass
---
+++ customer-support-system/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages,setup
+
+setup(name='e-commerce-bot',
+      version='0.0.1',
+      author='anurag',
+      packages=find_packages(),
+      install_requrires=['langchain-astradb','langchain'])
